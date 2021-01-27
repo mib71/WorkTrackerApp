e2bba19e7271a3d072bec75c556981a82b3f0882
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-01-27 20:20:49 +0100

    Add filter Remove footer

--- WorkTracker/Pages/WorkItems/Index.cshtml.cs
+++ WorkTracker/Pages/WorkItems/Index.cshtml.cs
@@ -33,7 +33,40 @@ namespace WorkTracker.Pages.WorkItems
                 .Include(w => w.Priority)
                 .Include(w => w.Status).ToListAsync();
                         
-            ProjName = WorkItem.Select(p => p.Project.Name).FirstOrDefault().ToString();                       
+            ProjName = await _context.Projects.Where(p => p.ProjectId == id).Select(p => p.Name).FirstOrDefaultAsync();            
+        }
+
+        public void OnPost(int id, string status)
+        {
+            if (status == "all")
+            {
+                WorkItem = _context.WorkItems.Include(w => w.Project)
+                    .Where(w => w.ProjectId == id)
+                    .Include(w => w.Priority)
+                    .Include(w => w.Status).ToList();
+            }
+            else if (status == "inp")
+            {
+                WorkItem = _context.WorkItems.Include(w => w.Project)
+                    .Where(w => w.ProjectId == id)
+                    .Include(w => w.Priority)
+                    .Include(w => w.Status).Where(w => w.StatusId == "In Progress").ToList();
+            }
+            else if (status == "don")
+            {
+                WorkItem = _context.WorkItems.Include(w => w.Project)
+                    .Where(w => w.ProjectId == id)
+                    .Include(w => w.Priority)
+                    .Include(w => w.Status).Where(w => w.StatusId == "Done").ToList();
+            }
+            else if (status == "tod")
+            {
+                WorkItem = _context.WorkItems.Include(w => w.Project)
+                    .Where(w => w.ProjectId == id)
+                    .Include(w => w.Priority)
+                    .Include(w => w.Status).Where(w => w.StatusId == "To Do").ToList();
+            }
+
         }
     }
 }
